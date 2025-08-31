37e77c778ca4023c8165fcb291e68158c8d47f55
Shah Jalal <rjshahjalal132@gmail.com>
2025-08-31 12:09:07 +0600

    remove put_program_logs function from root file

--- wholesaler-integration.php
+++ wholesaler-integration.php
@@ -127,30 +127,3 @@ function calculate_product_price_with_margin( $wholesaler_price, $brand ) {
 
     return number_format( $product_regular_price, 2, '.', '' );
 }
-
-// Function to append data to a log file
-function put_program_logs( $data ) {
-
-    // Ensure the directory for logs exists
-    $directory = __DIR__ . '/program_logs/';
-    if ( !file_exists( $directory ) ) {
-        // Use wp_mkdir_p instead of mkdir
-        if ( !wp_mkdir_p( $directory ) ) {
-            return "Failed to create directory.";
-        }
-    }
-
-    // Construct the log file path
-    $file_name = $directory . 'program_logs.log';
-
-    // Append the current datetime to the log entry
-    $current_datetime = gmdate( 'Y-m-d H:i:s' ); // Use gmdate instead of date
-    $data             = $data . ' - ' . $current_datetime;
-
-    // Write the log entry to the file
-    if ( file_put_contents( $file_name, $data . "\n\n", FILE_APPEND | LOCK_EX ) !== false ) {
-        return "Data appended to file successfully.";
-    } else {
-        return "Failed to append data to file.";
-    }
-}
